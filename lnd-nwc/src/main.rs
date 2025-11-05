c3f8cda8b5a4590d086a310b2a819a3919b2ee9a
arnaud <arnaud@secondlayer.co>
2025-11-04 19:24:39 -0600

    main makes a simple GRPC call

--- lnd-nwc/src/main.rs
+++ lnd-nwc/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+mod rpc;
+
+use crate::rpc::lnrpc::GetInfoRequest;
+use crate::rpc::lnrpc::lightning_client::LightningClient;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = LightningClient::connect("http://192.168.1.8:10009").await?;
+    let request = tonic::Request::new(GetInfoRequest {});
+    let response = client.get_info(request).await?;
+    println!("RESPONSE={:?}", response);
+
+    Ok(())
 }
