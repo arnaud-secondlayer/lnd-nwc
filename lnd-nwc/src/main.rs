bc57a02aa9c23cc7bd19d9c80608a5ee880bd6a9
arnaud <arnaud@secondlayer.co>
2025-11-08 14:26:07 -0600

    Use `lnd_grpc_rust`

--- lnd-nwc/src/main.rs
+++ lnd-nwc/src/main.rs
@@ -1,14 +1,32 @@
-mod rpc;
-
-use crate::rpc::lnrpc::GetInfoRequest;
-use crate::rpc::lnrpc::lightning_client::LightningClient;
+use std::fs;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = LightningClient::connect("http://192.168.1.8:10009").await?;
-    let request = tonic::Request::new(GetInfoRequest {});
-    let response = client.get_info(request).await?;
-    println!("RESPONSE={:?}", response);
+    let cert_bytes = fs::read("tls.cert").expect("FailedToReadTlsCertFile");
+    let mac_bytes = fs::read("admin.macaroon").expect("FailedToReadMacaroonFile");
+
+    let cert = buffer_as_hex(cert_bytes);
+    let macaroon = buffer_as_hex(mac_bytes);
+    let socket = "192.168.1.8:10009".to_string();
+
+    let mut client = lnd_grpc_rust::connect(cert, macaroon, socket)
+        .await
+        .expect("failed to connect");
+
+    let info = client
+        .lightning()
+        .get_info(lnd_grpc_rust::lnrpc::GetInfoRequest {})
+        .await
+        .expect("failed to get info");
+
+    println!("{:#?}", info);
 
     Ok(())
+}
+
+fn buffer_as_hex(bytes: Vec<u8>) -> String {
+    return bytes
+        .iter()
+        .map(|b| format!("{:02x}", b))
+        .collect::<String>();
 }
